1fa000da070adb3d091a044d74be8e55eb1bc604
reymart0132 <reymart.bolasoc@gmail.com>
2023-05-16 09:53:16 +0800

    cleaning of files
    clean

--- expiredAccounting.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/ecle/resource/php/class/core/init.php';
-expireAccounting();
-header('Location:accounting.php');
-?>
--- sendmails.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/ecle/resource/php/class/core/init.php';
-sendmailLibrary();
-sendmailDean();
-sendmailAccounting();
-header('Location:registrar.php');
-?>
