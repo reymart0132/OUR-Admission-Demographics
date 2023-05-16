8e3a041ada3f5d06d5919add82b29826e267ff6b
reymart0132 <reymart.bolasoc@gmail.com>
2023-05-16 09:41:38 +0800

    database config
    database config

--- resource/php/class/config.php
+++ resource/php/class/config.php
@@ -2,22 +2,22 @@
 
 class config{
     //hostinger
-    // private $user = 'ceumnlre_root';
-    // private $password = 'Eg5c272klko5';
+     private $user = 'ceumnlre_root';
+     private $password = 'Eg5c272klko5';
 
     //local
-    private $user = 'root';
-    private $password = '';
+    //private $user = 'root';
+    //private $password = '';
     
     public $pdo = null;
 
     public function con(){
         try {
             // hostinger
-            // $this->pdo = new PDO('mysql:host=109.106.254.187;dbname=ceumnlre_ecle', $this->user, $this->password);
+             $this->pdo = new PDO('mysql:host=109.106.254.187;dbname=ceumnlre_maps', $this->user, $this->password);
 
             // localhost
-            $this->pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=ceumnlre_maps', $this->user, $this->password);
+            //$this->pdo = new PDO('mysql:host=127.0.0.1:3306;dbname=ceumnlre_maps', $this->user, $this->password);
             } catch (PDOException $e) {
                 die($e->getMessage());
         }
